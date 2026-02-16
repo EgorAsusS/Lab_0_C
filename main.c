4aa1adb0c3c02ec7f7175da7836e0eb9ae8df737
Egor <egor.firsov.egor@yandex.ru>
2026-02-16 16:06:44 +0700

    Edit algorythm
    Cause this algorythm do not work good with boundary values

--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 
 int main()
 {
-    int array[szz] = {5,5,7,3,3,2,2,1,1,10};
+    int array[szz] = { 5,5,7,3,3,2,2,1,1,10 };
     int max = array[0];
     for(int i=1;i < szz;i++)
         if(array[i] > max) max = array[i];
     int notExist = max+1;
     int countEl=0;
+    if(notExist == -2147483647-1){
+        for(int i = 0; i < szz; i++){
+            if (array[i] == notExist){
+                countEl++;
+                i = szz;
+            }
+        }
+    }
     for(int i=0;i < szz;i++)
     {
         int jx = i+1;
